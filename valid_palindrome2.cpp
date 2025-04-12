466c4caba3079bd15ed77d88a29972f56b2180b2
Aryan4226 <aryanshambharkar22@gmail.com>
2025-04-13 02:12:00 +0530

    valid_palindrome2.cpp

---
+++ valid_palindrome2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindromeRange(string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+               
+                return isPalindromeRange(s, left + 1, right) || isPalindromeRange(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
